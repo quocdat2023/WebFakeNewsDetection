8814a4e197f0bd7bc62abacd1dd3f532a99fc281
quocdat2023 <quocdat51930@gmail.com>
2023-04-03 22:04:02 +0700

    f

--- Web Fake News/static/js/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let sidenavbar = document.querySelector('.side-navbar');
-let content = document.querySelector('.content');
- 
-menu.onclick = () => {
-    sidenavbar.classList.toggle('active');
-    content.classList.toggle('active');
-}
