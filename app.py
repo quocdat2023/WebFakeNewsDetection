35180dccbc2c1f75bf65e6489087c5c6576d3681
Nguyễn Quốc Đạt <125524178+quocdat2023@users.noreply.github.com>
2023-04-07 20:35:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://fakenewsdetection-98ld.onrender.com/callback"
 )
 
 def allowed_file(filename):
