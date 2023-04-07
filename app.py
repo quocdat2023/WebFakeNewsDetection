ac37eaacb1d04fdc3e9a2e6017444bdfa9e85ef0
Nguyễn Quốc Đạt <125524178+quocdat2023@users.noreply.github.com>
2023-04-07 19:03:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from pyvi import ViTokenizer
 # from vncorenlp import VnCoreNLP
 from bs4 import BeautifulSoup
 import random
+from gevent.pywsgi import WSGIServer
 
 
 app = Flask(__name__) 
@@ -380,4 +381,9 @@ def loginadmin():
         return redirect('login.html')
 
 
-app.run(port=5000,debug=True)
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
