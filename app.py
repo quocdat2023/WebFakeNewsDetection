a95fa9336333741ec608577a4f95ec30eeb2b21e
Nguyễn Quốc Đạt <125524178+quocdat2023@users.noreply.github.com>
2023-04-17 16:36:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -502,9 +502,9 @@ def loginAdmin():
 @app.route('/admin/fake_news_published/')
 def fake_news_published():
     fake = db.forum_report.find({"Status": 1,"Label":0})
-    name_session = session.get('users')
-    if name_session is None:
-        return redirect(url_for('index'))
+   name_session = session.get('users')
+    if name_session =="":
+           return redirect(url_for('index'))
     else:
         return render_template('fake_news_published.html',fake=fake)
 
@@ -513,8 +513,8 @@ def fake_news_published():
 def real_news_published():
     real = db.forum_report.find({"Status": 1,"Label":1})
     name_session = session.get('users')
-    if name_session is None:
-        return redirect(url_for('index'))
+    if name_session =="":
+           return redirect(url_for('index'))
     else:
         return render_template('real_news_published.html',real=real)
 
@@ -523,8 +523,8 @@ def real_news_published():
 def uncensored_new():
     uncensored_new = db.forum_report.find({"Status": 0})
     name_session = session.get('users')
-    if name_session is None:
-        return redirect(url_for('index'))
+    if name_session =="":
+           return redirect(url_for('index'))
     else:
         return render_template('uncensored_new.html',uncensored_new=uncensored_new)
 
@@ -533,8 +533,8 @@ def uncensored_new():
 def censored_new():
     censored_new = db.forum_report.find({"Status": 1})
     name_session = session.get('users')
-    if name_session is None:
-        return redirect(url_for('index'))
+    if name_session =="":
+           return redirect(url_for('index'))
     else:
         return render_template('censored_new.html',censored_new=censored_new)
 
@@ -542,8 +542,8 @@ def censored_new():
 def deny_new():
     deny_new = db.forum_report.find({"Status": 2})
     name_session = session.get('users')
-    if name_session is None:
-        return redirect(url_for('index'))
+    if name_session =="":
+           return redirect(url_for('index'))
     else:
         return render_template('deny_new.html',deny_new=deny_new)
 
@@ -593,7 +593,11 @@ def dashboard():
     ]
         # Thực thi pipeline để đếm số lượng bản ghi và nhóm chúng
     result = list(db.forum_report.aggregate(pipeline))
-    return render_template('dashboard.html', uncensored=uncensored, result=result, count_uncensored=count_uncensored, count_censored=count_censored, count_real=count_real, count_fake=count_fake)
+    name_session = session.get('users')
+    if name_session =="":
+           return redirect(url_for('index'))
+    else:
+        return render_template('dashboard.html', uncensored=uncensored, result=result, count_uncensored=count_uncensored, count_censored=count_censored, count_real=count_real, count_fake=count_fake)
 
 
 if __name__ == '__main__':
